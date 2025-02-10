fe070c81df8a23b6861c8c5c577a70bb12c6f698
José Chi Quintal <a17000597@alumnos.uady.mx>
2025-02-10 12:32:32 -0600

    Funcion para cargar excel agregada

--- Dashboards/Form1.cs
+++ Dashboards/Form1.cs
@@ -1,9 +1,32 @@
 using ScottPlot;
+using SpreadsheetLight;
 
 namespace Dashboards
 {
     public partial class Form1 : Form
     {
+        string pathFile = AppDomain.CurrentDomain.BaseDirectory + "vacantes.xlsx";
+        SLDocument document;
+
+        // Cargar documento de Excel
+       
+private void loadDocument()
+        {
+            int index = 1;
+            document = new SLDocument(pathFile);
+            while (!string.IsNullOrEmpty(document.GetCellValueAsString(index, 1)))
+            {
+                List<string> rowValues = new List<string>();
+                for (int col = 1; col <= 18; col++)
+                {
+                    string cellValue = document.GetCellValueAsString(index, col);
+                    rowValues.Add(cellValue);
+                }
+                
+                index++;
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -15,10 +38,8 @@ namespace Dashboards
             double[] dataX = { 1, 2, 3, 4, 5 };
             double[] dataY = { 1, 4, 9, 16, 25 };
 
-
             formsPlot1.Plot.Add.Scatter(dataX, dataY);
             formsPlot1.Refresh();
-
         }
     }
 }
